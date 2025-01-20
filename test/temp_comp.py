4bcca8e116e2f3d20076b2775216907561cb6b92
Azcobu <81782124+Azcobu@users.noreply.github.com>
2025-01-20 13:53:33 +1030

    Update temp_comp.py

--- test/temp_comp.py
+++ test/temp_comp.py
@@ -6,13 +6,12 @@ import time
 
 dht22 = adafruit_dht.DHT22(board.D4)  
 
-
 def read_sht41():
     serial_port = '/dev/ttyACM0'
     baud_rate = 9600
 
     try:
-        with serial.Serial(serial_port,baud_rate, timeout=1) as ser:
+        with serial.Serial(serial_port, baud_rate, timeout=1) as ser:
             line = ser.readline().decode('utf-8').strip()
             if not line:
                 print("SHT41: no data received from sensor")
@@ -27,7 +26,7 @@ def read_sht41():
 
 def read_dht22():
     try:
-        temperature = dht_sensor.temperature
+        temperature = dht22.temperature
 
         if temperature is not None:
             return temperature
@@ -41,16 +40,16 @@ def read_dht22():
 def main():
 
     while True:
-    try:
-        trinkey_temp = read_sht41()
-        dht22_temp = read_dht22()
-        print(f'Trinkey: {trinkey_temp}, DHT22: {dht22_temp}')
-        time.sleep(5)
-    except KeyboardInterrupt:
-        print("\nExiting...")
-    finally:
-        dht22.exit()  # Clean up DHT22 resources
-        sht41_serial.close()  # Close serial connection
+        try:
+            trinkey_temp = read_sht41()
+            dht22_temp = read_dht22()
+            print(f'Trinkey: {trinkey_temp}, DHT22: {dht22_temp}')
+            time.sleep(5)
+        except KeyboardInterrupt:
+            print("\nExiting...")
+        finally:
+            dht22.exit()  # Clean up DHT22 resources
+            #sht41_serial.close()  # Close serial connection
 
 if __name__ == '__main__':
     main()
