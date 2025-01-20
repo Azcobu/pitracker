60546348c27790c4a7a1308e278bc54e8e2cc3db
Azcobu <81782124+Azcobu@users.noreply.github.com>
2025-01-20 14:06:47 +1030

    Create sht41.py

---
+++ test/sht41.py
@@ -0,0 +1,29 @@
+    import time
+    import serial
+    
+def read_sht41():
+
+    try:
+        with serial.Serial('/dev/ttyACM0', 9600, timeout=2) as ser:
+            line = ser.readline().decode('utf-8').strip()
+            if not line:
+                print("SHT41: no data received from sensor")
+                return 'N/A'
+
+            _, temp, humid, touch = line.split(',')
+            return float(temp)
+
+    except Exception as err:
+        print(f'SHT41 error:{err}')
+        return 'N/A'
+
+def main():
+    while True:
+        try:
+            temp = read_sensor()
+            print(f'{}temp')
+        except Exception as err:
+            print(f'SHT41 error:{err}')
+
+if __name__ == '__main__':
+    main()
